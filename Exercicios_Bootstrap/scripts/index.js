77ac431ed46f0d4d39300ad48a2f6d7f869a86fe
AndrehW27 <andrehwillian@hotmail.com>
2020-09-28 08:08:49 -0300

    footer e ajustes

---
+++ Exercicios_Bootstrap/scripts/index.js
@@ -0,0 +1,9 @@
+function openWhats () {
+    let numero = document.querySelector('#numero');
+    numero.style.display = 'flex';
+}
+
+function closeWhats () {
+    let numero = document.querySelector('#numero');
+    numero.style.display = 'none';
+}
